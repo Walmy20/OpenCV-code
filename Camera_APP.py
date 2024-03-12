b23155da9e5d44b52dd4f2fb6c4db7b2cea37994
Walmy20 <74880546+Walmy20@users.noreply.github.com>
2024-03-12 16:15:17 -0400

    Update Camera_APP.py

--- Camera_APP.py
+++ Camera_APP.py
@@ -5,7 +5,7 @@ os.add_dll_directory(gstreamer_path)
 import cv2
 import pandas as pd
 import subprocess
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QGridLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QSizePolicy, QComboBox, QVBoxLayout
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
@@ -21,11 +21,9 @@ def get_codec(camera):
         if output == '' and process.poll() is not None:
             break
         if 'H264' in output:
-            print('Encoding H264')
             process.terminate()
             return 'H264'
         elif 'H265' in output:
-            print('Encoding H265')
             process.terminate()
             return 'H265'
     print(f"H.264 or H.265 decoder not found! Skipping RTSP Url: {camera}")
@@ -34,6 +32,29 @@ def get_codec(camera):
 def set_cap(camera):
      codec = get_codec(camera)
      if codec == 'H264':
+        print('Encoding H264')
+        gst_str = ("rtspsrc location=" + camera + " latency=0 ! "
+            "rtph264depay ! h264parse ! decodebin ! "
+            "videoscale ! video/x-raw,width=640,height=480 ! "
+            "videorate !  video/x-raw,framerate=24/1 !"
+                "videoconvert ! appsink drop=true")
+        return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+     elif codec == 'H265':
+        print('Encoding H265')
+        gst_str = ("rtspsrc location=" + camera + " latency=0 ! "
+            "rtph265depay ! h265parse ! decodebin ! "
+            "videoscale ! video/x-raw,width=640,height=480 ! "
+            "videorate !  video/x-raw,framerate=24/1 !"
+                "videoconvert ! appsink drop=true")
+        return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+     else:
+        print(f"Unsupported video encoding for RTSP Url: {camera}")
+        return None
+
+def set_cap_high(camera):
+     codec = get_codec(camera)
+     if codec == 'H264':
+        print('High-Res Encoding H264')
         gst_str = ("rtspsrc location=" + camera + " latency=0 ! "
             "rtph264depay ! h264parse ! decodebin ! "
             "videoscale ! video/x-raw,width=1280,height=720 ! "
@@ -41,6 +62,7 @@ def set_cap(camera):
                 "videoconvert ! appsink drop=true")
         return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
      elif codec == 'H265':
+        print('High-Res Encoding H265')
         gst_str = ("rtspsrc location=" + camera + " latency=0 ! "
             "rtph265depay ! h265parse ! decodebin ! "
             "videoscale ! video/x-raw,width=1280,height=720 ! "
@@ -51,18 +73,19 @@ def set_cap(camera):
         print(f"Unsupported video encoding for RTSP Url: {camera}")
         return None
 
-
 class VideoThread(QThread):
     changePixmap = pyqtSignal(QPixmap)
 
-    def __init__(self, camera, label):
+    def __init__(self, camera, label, high_res=False):
         QThread.__init__(self)
         self.camera = camera
         self.label = label
+        self.high_res = high_res
 
     def run(self):
         print("VideoThread started") 
-        cap = set_cap(self.camera)
+        cap = set_cap_high(self.camera) if self.high_res else set_cap(self.camera)
+        self.cap = cap
         if cap is None:
             # Create a black image if the RTSP URL is incorrect
             rgb_image = np.zeros((480, 640, 3), dtype=np.uint8)
@@ -74,16 +97,33 @@ class VideoThread(QThread):
             return
         while True:
             ret, frame = cap.read()
+
+            # This if was created for Person tracking 
+            if self.high_res and ret:
+                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgb_image.shape
+                bytes_per_line = ch * w
+                convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                p = QPixmap.fromImage(convert_to_Qt_format)
+                self.changePixmap.emit(p)
+                
             if ret:
                 rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 h, w, ch = rgb_image.shape
                 bytes_per_line = ch * w
                 convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-                p = QPixmap.fromImage(convert_to_Qt_format)#p = convert_to_Qt_format #p = convert_to_Qt_format.scaled(640, 480, Qt.KeepAspectRatio)
+                p = QPixmap.fromImage(convert_to_Qt_format)
                 self.changePixmap.emit(p)
     @pyqtSlot(QPixmap)
     def updatePixmap(self, image):
         self.label.setPixmap(image)
+    
+    def stop(self):
+        if self.cap is None: # If cap is None just Stop the Qthread
+            self.quit()
+        else:
+            self.cap.release()  # Stop the video capture
+            self.quit()  # Stop the QThread
 
 
 class App(QWidget):
@@ -91,13 +131,32 @@ class App(QWidget):
         super().__init__()
         self.title = 'Cameras CWD-Video'
         self.full_screen = True  # Add a flag to indicate full screen mode
+        self.path = r"C:\\Users\\walmym\\Desktop\\Downloads\\Scripts\\Camera App\\Camera.xlsx"
+        self.df = pd.read_excel(self.path)
+        #self.cameras = self.df['Cameras'].dropna().tolist()
+        self.groups = {group: self.df[group].dropna().tolist() for group in ["Group1", "Group2", "Group3"]}  # Define your groups here
+        self.cameras = self.groups["Group1"]
         self.initUI()
 
     def initUI(self):
         print("initUI called") 
         self.setWindowTitle(self.title)
+        #layout = QGridLayout()
+        #self.setLayout(layout)
+
+            # Create a vertical box layout and add it to the window
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        # Create a combo box for group selection
+        self.comboBox = QComboBox(self)
+        self.comboBox.addItems(self.groups.keys())
+        self.comboBox.currentIndexChanged.connect(self.update_cameras)
+        vbox.addWidget(self.comboBox)  # Add the combo box to the vertical box layout
+
+        # Create a grid layout for the labels
         layout = QGridLayout()
-        self.setLayout(layout)
+        vbox.addLayout(layout)  # Add the grid layout to the vertical box layout
 
         # Create labels for displaying the video
         self.labels = [QLabel(self) for _ in range(6)]
@@ -108,13 +167,9 @@ class App(QWidget):
             label.mousePressEvent = self.get_label_click_handler(i)  # Add mouse click event handler
             layout.addWidget(label, i // 3, i % 3)  # Arrange labels in a 4x3 grid
 
-        # Lists of your camera RTSP URLs for different areas
-        path = r"C:\\Users\\walmym\\Desktop\\Downloads\\Scripts\\Camera App\\Camera.xlsx"
-        df = pd.read_excel(path)
-        cameras = df['Cameras'].dropna().tolist()
 
         # Create and start video threads for each camera
-        self.threads = [VideoThread(camera, label) for camera, label in zip(cameras, self.labels)]
+        self.threads = [VideoThread(camera, label) for camera, label in zip(self.cameras, self.labels)]
         for thread in self.threads:
             thread.changePixmap.connect(thread.updatePixmap)
             thread.start()
@@ -124,6 +179,12 @@ class App(QWidget):
         
     def get_label_click_handler(self, i):
         def handler(event):
+            self.threads[i].stop()  # Stop the previous thread
+
+            # Start a new high-resolution thread
+            self.threads[i] = VideoThread(self.cameras[i], self.labels[i], high_res=True)
+            self.threads[i].changePixmap.connect(self.threads[i].updatePixmap)
+            self.threads[i].start()
             # Hide all labels
             for label in self.labels:
                 label.hide()
@@ -131,14 +192,39 @@ class App(QWidget):
             self.labels[i].show()
             self.full_screen = False  # Set full screen flag to False
         return handler
+    
+    def update_cameras(self, index):
+        group_name = self.comboBox.itemText(index)
+        self.cameras = self.groups[group_name]
+        # Stop the previous threads and start new ones with the selected group cameras 
+        for thread in self.threads:
+            thread.stop()
+
+        self.threads = [VideoThread(camera, label, high_res=False) for camera, label in zip(self.cameras, self.labels)]
+
+        for thread in self.threads:
+            thread.changePixmap.connect(thread.updatePixmap)
+            thread.start()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:  # If the 'Escape' key is pressed
             if not self.full_screen:  # If the application is not in full screen mode
+                for thread in self.threads:
+                    thread.stop()  # Stop the previous threads
+                
+                # Restart all threads with normal resolution
+                self.threads = [VideoThread(camera, label, high_res=False) for camera, label in zip(self.cameras, self.labels)]
+
+                for thread in self.threads:
+                    thread.changePixmap.connect(thread.updatePixmap)
+                    thread.start()
                 # Show all labels
                 for label in self.labels:
                     label.show()
                 self.full_screen = True  # Set full screen flag to True
+
+                # Show the combo box
+                #self.comboBox.show()
         super().keyPressEvent(event)
 
        
